35a08685f7e7b27077dc521de27a6867a98e6a26
MrsGarisson <ksushata@gmail.com>
2013-03-19 12:51:15 +0300

    unformatted

--- weather.sh
+++ weather.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-# declare STRING variable
-STRING="Hello World"
-#print variable on a screen
-echo $STRING
+i=$(< config.ini)
+url="pda.pogoda.by";
+ping -q -c1 $url | grep recieved | awk '{print $1}'
+if  [ $? -eq 0 ]
+then
+	echo "ok"
+
+    		wget -q -O- $url | iconv -f cp1251 -t utf8>tmp.txt>> tmp.txt
+	#awk '/Температура/ {print}' tmp.txt 
+	echo `cat ./tmp.txt | grep -A 1 "погода фактическая"`
+    	sleep $i
+	rm tmp.txt
+else
+	echo "no connection"
+fi
+
+
