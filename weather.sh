d81a57caa841c0b6530acaea59ca41797a46513c
MrsGarisson <ksushata@gmail.com>
2013-03-19 13:50:48 +0300

    withoutCycle

--- weather.sh
+++ weather.sh
@@ -4,13 +4,16 @@ url="pda.pogoda.by";
 ping -q -c1 $url | grep recieved | awk '{print $1}'
 if  [ $? -eq 0 ]
 then
-    	wget -q -O- $url | iconv -f cp1251 -t utf8>tmp.txt>> tmp.txt
-	weather=`cat ./tmp.txt | grep -A 1 "погода фактическая"| sed 's/<br>/\n/g'|sed 's/<[^>]*>/ /g'`
-	echo -e "$weather"
-	echo
-	echo
-    	sleep $i
-	rm tmp.txt
+	while true  
+	do  	
+		wget -q -O- $url | iconv -f cp1251 -t utf8>tmp.txt>> tmp.txt
+		weather=`cat ./tmp.txt | grep -A 1 "погода фактическая"| sed 's/<br>/\n/g'|sed 's/<[^>]*>/ /g'`
+		echo -e "$weather"
+		echo
+		echo
+    		sleep $i
+		rm tmp.txt
+	done
 else
 	echo "no connection"
 fi
